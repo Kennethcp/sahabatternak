74e8e42cc70f32a02307fe167c428a5cc5859e43
Kenneth Christopher Prasetya <88609786+Kennethcp@users.noreply.github.com>
2025-11-17 09:06:34 +0700

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gjrcwxodiizcwizkxfkk.supabase.co'; // Replace with your Supabase project URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqcmN3eG9kaWl6Y3dpemt4ZmtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3MjY2NzMsImV4cCI6MjA0ODMwMjY3M30.T1vEjCr5D9LoQTbyD3nEZXdoXu16DsfRQLi4Ot1EIyo'; // Replace with your Supabase anon key
+const supabaseUrl = 'https://vhzjybeixtxedosqyrlz.supabase.co'; // Replace with your Supabase project URL
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoemp5YmVpeHR4ZWRvc3F5cmx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzMjU1NjcsImV4cCI6MjA3ODkwMTU2N30.PUXzX4NgpKiyO1chDS_Oy_tMxEKtWRbIYwesMjFEL5s'; // Replace with your Supabase anon key
 
 export const fetchDataByDate = async (date) => {
     const { data, error } = await supabase.from('events').select('*').eq('date', date);
